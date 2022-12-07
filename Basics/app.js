8474a7a4ee6b70dbaeb33ba69f7649de7a2ec972
Royanosh <68832856+Royanosh@users.noreply.github.com>
2022-12-07 15:27:47 +0530

    Request Body Parsing

--- Basics/app.js
+++ Basics/app.js
@@ -1,8 +1,36 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   // console.log(req);
-  console.log(req.url, req.method, req.headers);
+  // console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter the Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message' ><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   // process.exit()
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
